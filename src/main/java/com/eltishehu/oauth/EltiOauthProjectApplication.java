138ec6cbb3bbeb41f538fa22b5c928bcf8a894e2
Elti Shehu <shehuelti@gmail.com>
2018-11-12 15:13:33 +0100

    Initial project commit

---
+++ src/main/java/com/eltishehu/oauth/EltiOauthProjectApplication.java
@@ -0,0 +1,12 @@
+package com.eltishehu.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EltiOauthProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EltiOauthProjectApplication.class, args);
+    }
+}
---
+++ src/test/java/com/eltishehu/oauth/EltiOauthProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eltishehu.oauth;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EltiOauthProjectApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
